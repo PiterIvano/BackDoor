01188b832669295c68988f34f74e901235088105
piter <68443246+PiterIvano@users.noreply.github.com>
2021-11-19 21:41:30 +0100

    1.1

--- login.php
+++ login.php
@@ -11,7 +11,7 @@ if (isset($_SESSION['name'])){
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>CMD</title>
+        <title>Backdor</title>
         <style>
             body{
                 background-color: grey;
